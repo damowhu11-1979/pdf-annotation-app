bfce1c4197efbd44e0cb583f644b91ab353d122b
damowhu11-1979 <damowhu11@gmail.com>
2025-12-22 10:58:40 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './styles.css'
-
-createRoot(document.getElementById('root')).render(<App />)
+import './index.css' // <--- THIS LINE IS MANDATORY
