3d6ec90b13772a52357f3038402a577b3dd653c2
damowhu11-1979 <damowhu11@gmail.com>
2025-12-22 16:24:56 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css' // <--- THIS LINE IS MANDATORY
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+
+// A simple error boundary so you see the crash on-screen (not blank)
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { err: null };
+  }
+  static getDerivedStateFromError(err) {
+    return { err };
+  }
+  componentDidCatch(err) {
+    console.error("App crashed:", err);
+  }
+  render() {
+    if (this.state.err) {
+      return (
+        <div style={{ padding: 16, fontFamily: "sans-serif" }}>
+          <h2 style={{ color: "crimson" }}>App crashed</h2>
+          <pre style={{ whiteSpace: "pre-wrap" }}>
+            {String(this.state.err?.stack || this.state.err)}
+          </pre>
+        </div>
+      );
+    }
+    return this.props.children;
+  }
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </React.StrictMode>
+);
