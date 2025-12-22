1841e6354fdea853be35d822ebce304aad703b87
damowhu11-1979 <damowhu11@gmail.com>
2025-12-22 12:23:37 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // REPLACE 'pdf-editor' below with your ACTUAL repository name from GitHub!
+  base: '/pdf-editor/', 
 })
