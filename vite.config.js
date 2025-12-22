1e55e588ee5a600acf5b64f4116f625fcd61d3ed
damowhu11-1979 <damowhu11@gmail.com>
2025-12-22 10:32:38 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+git add .
+git commit -m "Fix base path for GitHub Pages"
+git push
 export default defineConfig({
   plugins: [react()],
   // MUST match your repo name (case-sensitive)
