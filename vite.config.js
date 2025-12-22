13cf43e01089127d266e6c6ee893e375c3ea0952
damowhu11-1979 <damowhu11@gmail.com>
2025-12-22 15:04:46 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // REPLACE 'https://damowhu11-1979.github.io/pdf-annotation-app/', 
+  // REPLACE '/pdf-annotation-app/', 
 })
