92b64cd54bd92b85657ade6ee69094d1731e31e4
damowhu11-1979 <damowhu11@gmail.com>
2025-12-23 14:27:48 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pdf-annotation-app/', // Matches your repo name exactly
+  // "./" is the safest setting. 
+  // It allows the app to find its files on Localhost AND GitHub Pages automatically.
+  base: './',
 })
